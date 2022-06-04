a190215564d07c40653c31964cec6afe5e47eaca
YadanarLin <85671303+YadanarLin@users.noreply.github.com>
2022-06-04 14:01:31 +0900

    details

--- hack/models.py
+++ hack/models.py
@@ -6,7 +6,6 @@ class Article(models.Model): #One class definition means a table in the database
     title=models.CharField(max_length=200, default="")
     body=models.TextField()
     like=models.IntegerField(default=0)
-
     def __str__(self):
         return self.title
 
@@ -14,3 +13,4 @@ class Comment(models.Model):
     text = models.TextField()
     posted_at = models.DateTimeField(default=timezone.now)
     article = models.ForeignKey(Article, related_name='comments', on_delete=models.CASCADE)
+
--- hack/views.py
+++ hack/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from hack.models import Article
+from django.http import Http404
 
 # Create your views here.
 def index(request):
+    if request.method=="POST":
+        article=Article(title=request.POST["title"], body=request.POST["text"])
+        article.save()
+        return redirect(detail,article.id)
     context = {
         "articles": Article.objects.all()
         
     }
-    
     return render(request, 'hack/index.html', context)
 
-def detail(request,article_id):
+def detail(request, article_id):
+    try:
+        article=Article.objects.get(pk=article_id)
+    except Article.DoesNotExist:
+        raise Http404("Review does not exist")
+    if request.method=='POST':
+        comment=Article(article=article, text=request.POST['text'])
+        comment.save()
+
     context ={
-        "article_id" : article_id
+        "article" : article, #red article represents the one seen in html. blue one represents its value
+        "comments":article.comments.order_by('-posted_at')
     }
-    return render(request,"hack/page2.html",context)
+    
+    return render(request,'hack/page2.html',context)
