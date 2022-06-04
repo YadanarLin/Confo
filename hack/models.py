79feaa54d769e4ed12241a6ba7c8b7187627a416
YadanarLin <85671303+YadanarLin@users.noreply.github.com>
2022-06-04 13:22:01 +0900

    migration, models, html

--- hack/models.py
+++ hack/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Article(models.Model): #One class definition means a table in the database
+    title=models.CharField(max_length=200, default="")
+    body=models.TextField()
+    like=models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    text = models.TextField()
+    posted_at = models.DateTimeField(default=timezone.now)
+    article = models.ForeignKey(Article, related_name='comments', on_delete=models.CASCADE)
--- hack/views.py
+++ hack/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from hack.models import Article
 
 # Create your views here.
 def index(request):
-    return render(request, 'hack/index.html')
+    context = {
+        "articles": Article.objects.all()
+        
+    }
+    
+    return render(request, 'hack/index.html', context)
+
+def detail(request,article_id):
+    context ={
+        "article_id" : article_id
+    }
+    return render(request,"hack/page2.html",context)
